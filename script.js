52dc06a0df98fe8bcc2ae6b233e6220542976f9c
JohnteFresh <irungujohnson@yahoo.com>
2023-08-01 01:38:45 +0300

    Modify my js file

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 const menuBtn = document.querySelector('.menu');
 menuBtn.addEventListener('click', () => {
-  const mobilePopup = document.querySelector('#mobile_menu');
-  mobilePopup.classList.toggle('visible');
-  mobilePopup.style.display = 'block';
-  const restOfThePage = document.querySelector('#body');
-  restOfThePage.classList.toggle('visible');
-  restOfThePage.style.display = 'none';
-  z
+  const popUp = document.querySelector('#mobile_menu');
+  popUp.classList.toggle('show');
+  popUp.style.backgroundColor = 'white';
+  popUp.style.display = 'block';
+  const page = document.querySelectorAll('section, footer');
+  for (let i = 0; i < page.length; i += 1) {
+    page[i].classList.toggle('hide');
+  }
 });
 
+
 document.getElementById('x-btn').addEventListener('click', () => {
-  const mobilePopup = document.querySelector('#mobile_menu');
-  mobilePopup.classList.toggle('visible');
-  const restOfThePage = document.querySelectorAll('section, footer');
-  for (let i = 0; i < restOfThePage.length; i += 1) {
-    restOfThePage[i].classList.toggle('hidden');
+  const popUp = document.querySelector('#mobile_menu');
+  popUp.classList.toggle('show');
+  const page = document.querySelectorAll('section, footer');
+  for (let i = 0; i < page.length; i += 1) {
+    page[i].classList.toggle('hide');
   }
 });
 
 const linkBtn = document.querySelectorAll('.menu-bar a');
 for (let i = 0; i < linkBtn.length; i += 1) {
   linkBtn[i].addEventListener('click', () => {
-    const mobilePopup = document.querySelector('#mobile_menu');
-    mobilePopup.classList.toggle('visible');
-    const restOfThePage = document.querySelectorAll('section, footer');
-    for (let i = 0; i < restOfThePage.length; i += 1) {
-      restOfThePage[i].classList.toggle('hidden');
+    const popUp = document.querySelector('#mobile_menu');
+    popUp.classList.toggle('show');
+    const page = document.querySelectorAll('section, footer');
+    for (let i = 0; i < page.length; i += 1) {
+      page[i].classList.toggle('hide');
     }
   });
 }
