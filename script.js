f8a5891a5e8368413e5834d89f9fda2136a2fd3c
Drew246 <andrew.k.mccollin@gmail.com>
2023-07-31 10:22:11 -0400

    edit js file

--- script.js
+++ script.js
@@ -2,21 +2,14 @@ const menuBtn = document.querySelector('.menu');
 menuBtn.addEventListener('click', () => {
   const mobilePopup = document.querySelector('#mobile_menu');
   mobilePopup.classList.toggle('visible');
-  // mobilePopup.style.display = 'block';
-  const restOfThePage = document.querySelectorAll('section, footer');
-  for (let i = 0; i < restOfThePage.length; i += 1) {
-    restOfThePage[i].classList.toggle('hidden');
-  }
+  mobilePopup.style.display = 'block';
+  const restOfThePage = document.querySelector('#body');
+  restOfThePage.classList.toggle('visible');
+  restOfThePage.style.display = 'none';
+  z
 });
 
-document.getElementById('x-btn').addEventListener('click', () => {
-  const mobilePopup = document.querySelector('#mobile_menu');
-  mobilePopup.classList.toggle('visible');
-  const restOfThePage = document.querySelectorAll('section, footer');
-  for (let i = 0; i < restOfThePage.length; i += 1) {
-    restOfThePage[i].classList.toggle('hidden');
-  }
-});
+
 
 const linkBtn = document.querySelectorAll('.menu-bar a');
 for (let i = 0; i < linkBtn.length; i += 1) {
