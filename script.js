058c4a3e28595f00c82834c6e6b03451adaf3f32
JohnteFresh <irungujohnson@yahoo.com>
2023-07-31 16:00:17 +0300

    Add JS to project Modify html and css files

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-const menuBtn = document.querySelector(".menu-burger");
-menuBtn.addEventListener('click', function() {
-  const pop = document.querySelector("#mobile_menu");
-  pop.classList.toggle('visible');
-  const page = document.querySelectorAll('section, footer');
-  for (let i = 0; i < page.length; i++) {
-    page[i].classList.toggle('unvisible');
+const menuBtn = document.querySelector('.menu');
+menuBtn.addEventListener('click', () => {
+  const mobilePopup = document.querySelector('#mobile_menu');
+  mobilePopup.classList.toggle('visible');
+  // mobilePopup.style.display = 'block';
+  const restOfThePage = document.querySelectorAll('section, footer');
+  for (let i = 0; i < restOfThePage.length; i += 1) {
+    restOfThePage[i].classList.toggle('hidden');
   }
 });
 
-// document.getElementById('x-btn').addEventListener('click', function() {
-//   const pop = document.querySelector('.mobile_menu');
-//   pop.classList.toggle('visible');
-//   const page = document.querySelectorAll('section, footer');
-//   for (let i = 0; i < page.length; i++) {
-//     page[i].classList.toggle('unvisible');
-    
-//   }
-// });
+document.getElementById('x-btn').addEventListener('click', () => {
+  const mobilePopup = document.querySelector('#mobile_menu');
+  mobilePopup.classList.toggle('visible');
+  const restOfThePage = document.querySelectorAll('section, footer');
+  for (let i = 0; i < restOfThePage.length; i += 1) {
+    restOfThePage[i].classList.toggle('hidden');
+  }
+});
 
-// const otherBtn = document.querySelectorAll('.menu-bar a');
-// for (let i = 0; i < otherBtn.length; i += 1) {
-//   otherBtn[i].addEventListener('click', function() {
-//     const pop = document.querySelector('.mobile_menu');
-//     pop.classList.toggle('visible');
-//     const page = document.querySelectorAll('section, footer');
-//     for (let i = 0; i < page.length; i += 1) {
-//       page[i].classList.toggle('unvisible');
-//     }
-//   });
-  
-//  }
+const linkBtn = document.querySelectorAll('.menu-bar a');
+for (let i = 0; i < linkBtn.length; i += 1) {
+  linkBtn[i].addEventListener('click', () => {
+    const mobilePopup = document.querySelector('#mobile_menu');
+    mobilePopup.classList.toggle('visible');
+    const restOfThePage = document.querySelectorAll('section, footer');
+    for (let i = 0; i < restOfThePage.length; i += 1) {
+      restOfThePage[i].classList.toggle('hidden');
+    }
+  });
+}
