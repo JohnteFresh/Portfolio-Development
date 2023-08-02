44a6b5aac379d7a6fc90fdfe3e308f3688b7adb7
JohnteFresh <irungujohnson@yahoo.com>
2023-08-02 09:55:27 +0300

    Modify html and css file, Add JavaScript for Popup

---
+++ pop.js
@@ -0,0 +1,100 @@
+const popupDetails = [
+  {
+    name: "Project name goes here",
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    featured_image: "./icons/Rectangle 21.png",
+    image:"./icons/Group 105.png",
+    description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+
+    "Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    link_to_live_version: "https://johntefresh.github.io/Portfolio-Development/",
+    link_source: "https://github.com/JohnteFresh"
+  },
+  {
+    name: "Project name goes here",
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    featured_image: "./icons/Rectangle 21 (1).png",
+    image:"./icons/Group 105.png",
+    description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+
+    "Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    link_to_live_version: "https://johntefresh.github.io/Portfolio-Development/",
+    link_source: "https://github.com/JohnteFresh"
+  },
+  {
+    name: "Project name goes here",
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    featured_image: "./icons/Rectangle 21 (2).png",
+    image:"./icons/Group 105.png",
+    description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+
+    "Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    link_to_live_version: "https://johntefresh.github.io/Portfolio-Development/",
+    link_source: "https://github.com/JohnteFresh"
+  },
+  {
+    name: "Project name goes here",
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    featured_image: "./icons/Rectangle 21 (3).png",
+    image:"./icons/Group 105.png",
+    description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+
+    "Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    link_to_live_version: "https://johntefresh.github.io/Portfolio-Development/",
+    link_source: "https://github.com/JohnteFresh"
+  },
+  {
+    name: "Project name goes here",
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    featured_image: "./icons/Rectangle 21 (4).png",
+    image:"./icons/Group 105.png",
+    description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+
+    "Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    link_to_live_version: "https://johntefresh.github.io/Portfolio-Development/",
+    link_source: "https://github.com/JohnteFresh"
+  },
+  {
+    name: "Project name goes here",
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    featured_image: "./icons/Rectangle 21 (5).png",
+    image:"./icons/Group 105.png",
+    description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+
+    "Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi."],
+    link_to_live_version: "https://johntefresh.github.io/Portfolio-Development/",
+    link_source: "https://github.com/JohnteFresh"
+  }
+];
+
+
+
+
+
+//   const projectName = document.createElement('h2');
+//   projectName.textContent = popupDetails[i].name;
+//   projectName.classList.add('fonts', 'tech-color');
+//   popupCard.appendChild(projectName);
+
+//   const technologies = document.createElement('ul');
+//   technologies.innerHTML = `<li>${popupDetails[i].technologies[0]}</li>
+//   <li>${projectDetails[i].technologies[1]}</li>
+//   <li>${projectDetails[i].technologies[2]}</li>`;
+//   technologies.classList.add('fonts', 'tech-color');
+//   popupCard.appendChild(technologies);
+
+//   const imageBox = document.createElement('div');
+//   imageBox.classList.add('image-box');
+//   const featuredImage = document.createElement('img');
+//   featuredImage.src = popupDetails[i];
+//   featuredImage.classList.add('featured');
+//   imageBox.appendChild(featuredImage);
+
+
+
+//   // for (let j = 0; j < projectDetails[i].alternative_images.length; j += 1) {
+//   //   const smallImage = document.createElement('img');
+//   //   smallImage.src = projectDetails[i].alternative_images[j];
+//   //   smallImage.classList.add('small-image');
+//   //   imageBox.appendChild(smallImage);
+//   // }
