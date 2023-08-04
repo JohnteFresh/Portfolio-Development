c9768c99dde217dd5b431a6697e53667ac333786
JohnteFresh <irungujohnson@yahoo.com>
2023-08-04 10:11:36 +0300

    Add validation to email in form

--- validation.js
+++ validation.js
@@ -1,32 +1,15 @@
-const form = document.querySelector('#send-message')
-
-function messsage(input, message, type){
-  const msg = input.parentNode.querySelector('small:last-of-type');
-  const emailSuggest = `Replace it with ${form.elements.user-email.value.toLowerCase()}?`;
-  msg.innerText = message + emailSuggest;
-  input.classname = type ? 'success' : 'error';
-  return type
-}
-
-function showError(input, message){
-  return showMessage(input, message, false);
-}
-
-function emailValidate(input, invalidMsg){
-  const emailRegex = /[A-Z]/;
-  const email = input.value.trim();
-  if(emailRegex.test(email)){
-    return showError(input, invalidMsg);
-  }
-  return true;
-}
-const email_lowercase = 'Please enter you email in lowercase.';
+const form = document.getElementById('send-message');
+const emailInput = document.getElementById('user-email');
+const errorMsg = document.getElementById('error-text');
 
 form.addEventListener('submit', (event) => {
+  if (emailInput.value === emailInput.value.toLowerCase()) {
+    return true;
+  }
+  errorMsg.style.display = 'block';
   event.preventDefault();
-  
-  const validEmail = emailValidate(form.elements.user_email, email_lowercase);
-    if(validEmail){
-      form.submit();
-    }
+  return false;
 });
+
+document.getElementById('error-text').style.color = 'Red';
+document.getElementById('error-text').style.fontSize = '120%';
